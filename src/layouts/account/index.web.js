099c0d2e0e8142a2f6ba1325a17a9c173975b399
Awang Karisma <me@karisma.id>
2023-02-19 21:12:47 +0700

    feat: implement mobile specific feature to web

---
+++ src/layouts/account/index.web.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Account() {
+  return (
+    <iframe
+      src="https://google.com/search?igu=1"
+      frameBorder="0"
+      width="100%"
+      height="100%"
+      title="web"
+    />
+  );
+}
